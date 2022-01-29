414db8dc665abbb3060d515f54f65432e7925ec0
Roebuck98 <danielcorzogo@gmail.com>
2022-01-29 16:24:54 +0100

    Management Tutorial

---
+++ Assets/Scripts/SpawnPlayerManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnPlayerManager : MonoBehaviour
+{
+    public GameObject character;
+
+    public Transform spawnPointTuto;
+    public Transform spawnPointNoTuto;
+    void Awake()
+    {
+        IsTuto temp = null;
+        try
+        {
+            temp = GameObject.FindObjectOfType<IsTuto>();
+            if (temp.is_tuto)
+            {
+                //Debug.Log("Entra aqui");
+                character.transform.position = spawnPointTuto.position;
+            }
+            else
+            {
+                //Debug.Log("Entra aqui");
+                character.transform.position = spawnPointNoTuto.position;
+            }
+
+        }
+        finally
+        {
+            if (temp != null)
+                Object.DestroyImmediate(temp);
+        }
+    }
+
+    
+}
