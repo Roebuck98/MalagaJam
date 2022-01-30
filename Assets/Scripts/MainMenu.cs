e2a8a28610da9f63f288a016418fd9e1c885f315
J000KING <54002657+J000KING@users.noreply.github.com>
2022-01-30 11:59:51 +0100

    arreglo botones

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Main Scene");
+        SceneManager.LoadScene("MainScene");
     }
 
     public void QuitGame()
